b045b2887e620b3f7d7ec42daca8279a18fedbdd
champchawis <33029842+champchawis@users.noreply.github.com>
2020-09-01 12:56:55 +0700

    Update shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -13,12 +13,7 @@ declare namespace maqueenIR {
     //% block="on |%btn| button pressed" shim=maqueenIR::onPressEvent
     function onPressEvent(btn: RemoteButton, body: () => void): void;
 
-    /**
-     * initialises local variablesssss
-     */
-    //% blockId=ir_init
-    //% block="connect ir receiver to %pin" shim=maqueenIR::initIR
-    function initIR(pin: Pins): void;
+
 }
 
 // Auto-generated. Do not edit. Really.
